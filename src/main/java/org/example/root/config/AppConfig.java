874f124f959e6055bdc0d3bc7c2ba3926e51ca49
Shoxrux <komiljonovshoxrux19@gmail.com>
2025-10-05 11:48:04 +0500

    add project root Shoxrux

--- src/main/java/org/example/root/config/AppConfig.java
+++ src/main/java/org/example/root/config/AppConfig.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import okhttp3.OkHttpClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfig {
@@ -11,6 +12,10 @@ public class AppConfig {
     @Bean
     public OkHttpClient okHttpClient() {
         return new OkHttpClient();
+    }
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 
     @Bean
